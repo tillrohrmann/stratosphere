5fb5e29f50f608d323b369c17d0f993061321b56
StephanEwen <stephan.ewen@tu-berlin.de>
2014-03-25 20:43:05 +0100

    Remove redundant singleton global buffer pool instance. Better error messages for buffer size mismatches in the discarding data pool.

--- stratosphere-runtime/src/main/java/eu/stratosphere/runtime/io/network/bufferprovider/SerialSingleBuffePool.java
+++ stratosphere-runtime/src/main/java/eu/stratosphere/runtime/io/network/bufferprovider/SerialSingleBuffePool.java
@@ -43,7 +43,7 @@ public final class SerialSingleBuffePool implements BufferProvider, BufferRecycl
 			return this.buffer.duplicate();
 		}
 		else {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Requesting buffer with size " + minBufferSize + ". Pool's buffer size is " + this.bufferSize);
 		}
 	}
 
@@ -53,7 +53,7 @@ public final class SerialSingleBuffePool implements BufferProvider, BufferRecycl
 			return this.buffer.duplicate();
 		}
 		else {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Requesting buffer with size " + minBufferSize + ". Pool's buffer size is " + this.bufferSize);
 		}
 	}
 
